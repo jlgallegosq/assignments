c84877245c8fe844f1f46050e4f796dbcf63731c
Jose Gallegos Quezada <JLGallegos@MacBook-Air-de-Jose-3.local>
2019-01-22 03:07:02 -0500

    modifyin demo-website

--- sayhello.py
+++ sayhello.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+<<<<<<< HEAD
 #This program say hello
+=======
+# this is a sample comment
+# add your own comments to describe what this script does
+>>>>>>> cde239b0d7eedb33c0187d3f535044b23f100480
 import sys
 name = sys.stdin.read()
 print("Hello " + name + "!")
